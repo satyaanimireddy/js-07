5fdeffe73e1bdec70b57393e6837235a8e90cb1d
satya <satyaanimireddy00@gmail.com>
2024-05-02 09:16:48 +0530

    first commit

---
+++ 1.basics/index.js
@@ -0,0 +1,48 @@
+var a = 'sachin'
+
+console.log('value:', a, ',', 'datatype:', typeof a)
+var b = 40
+console.log('value:', b, ',', 'datatype:', typeof b)
+
+var c = true
+console.log('value:', c, ',', 'datatype:', typeof c)
+
+var d
+console.log('value:', d, ',', 'datatype:', typeof d)
+
+var e = null
+console.log('value:', e, ',', 'datatype:', typeof e)
+
+var f = {
+    name: "kohli",
+    age: 40
+}
+console.log('value:', f, ',', 'datatype:', typeof f)
+
+var g = [10, 20, 30]
+console.log('value:', g, ',', 'datatype:', typeof g)
+console.log(Array.isArray(g))
+
+
+var h = () => {
+    var x = 100
+    // console.log('test')
+}
+h()
+console.log('value:', h, ',', 'datatype:', typeof h)
+
+console.log(a, typeof a)
+
+//operators
+var x = 20
+var y = 10
+
+var z = x + y
+// console.log(z)
+console.log(x)
+// x += 10
+// x -= 10
+x /= 10
+console.log(x)
+
+
